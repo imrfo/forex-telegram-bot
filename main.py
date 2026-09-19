1dff4c9ef147b45c6c95c5b6c943aaaaf4ae9877
alireza faraji <112788924+imrfo@users.noreply.github.com>
2026-09-19 05:21:52 +0330

    Update main.py

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ def generate_persian_post(title, summary, category):
 #فارکس #{category.replace(' + ', ' #').replace('/', '_')}
 """
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
+        model="gemini-3.6-flash",
         contents=prompt
     )
     return response.text
